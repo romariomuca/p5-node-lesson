92f7687b990478e0aca34a414ad7810fc61db3ad
romariomuca <romario.muca@gmail.com>
2021-11-17 11:33:48 +0100

    added sketch

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
 console.log("I'm running ");
+
+let express = require("express"); //we are putting the libriary in a var
+
+let app = express(); //app is the reslut of running the express funtion created before
+
+let port = 3000;
+
+let server = app.listen(port); //the connection will happen on the port 3000
+
+console.log("server is running on http://localhost:" + port);
+
+app.use(express.static("public"));
