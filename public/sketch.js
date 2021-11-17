ec7340f7f0d0cb714370703e50a2fd4cdb615e0c
romariomuca <romario.muca@gmail.com>
2021-11-17 12:13:40 +0100

    added socket

--- public/sketch.js
+++ public/sketch.js
@@ -1,3 +1,11 @@
+let clientSocket = io(); //creo variabile clientSocket che è uguale io() che sta caricando la libreria grazie al html file
+
+clientSocket.on("connect", newConnection);
+
+function newConnection() {
+  console.log(clientSocket.id); //vedrò l'id di connessione del server
+}
+
 function setup() {
   createCanvas(400, 400);
 }
--- server.js
+++ server.js
@@ -10,4 +10,17 @@ let server = app.listen(port); //the connection will happen on the port 3000
 
 console.log("server is running on http://localhost:" + port);
 
-app.use(express.static("public"));
+app.use(express.static("public")); //il folder public non verrà cambiato over time ma è statico
+
+let serverSocket = require("socket.io"); //creo variabile che contiene la libreria socket
+
+let io = serverSocket(server); //Io = input/output diventa serverSocket con dentro la variabile che fa attivare express
+
+io.on("connection", newConnection); //se un messaggio connection arriva allora reagisce con la funzione newConnection
+//we have to add on the client side the connection part otherwise we it won't happen nothing - go on index and sketch
+//every time a client connect we will have the function newConnection ,
+//ogni persona che si connette io vedrò nel console log un nuovo newSocket.id
+
+function newConnection(newSocket) {
+  console.log(newSocket.id); //contains the unique id of client connection
+}
