8408dd74d693291fac28a167916dc26d02981b2f
romariomuca <romario.muca@gmail.com>
2021-11-17 12:55:24 +0100

    added broadcast

--- public/sketch.js
+++ public/sketch.js
@@ -1,16 +1,35 @@
-let clientSocket = io(); //creo variabile clientSocket che è uguale io() che sta caricando la libreria grazie al html file
+let clientSocket = io(); //io() is pre-build funciton it would be socket.io
+//creo variabile clientSocket che è uguale io() che sta caricando la libreria grazie al html file
 
-clientSocket.on("connect", newConnection);
+clientSocket.on("connect", newConnection); //when client connect to the server faccio funzione newConnection
+clientSocket.on("mouseBroadcast", newBroadcast); //client socket cìmouse broadcast quando lo riceve faccio funzione newBoradcast
 
 function newConnection() {
   console.log(clientSocket.id); //vedrò l'id di connessione del server
 }
 
+function newBroadcast(data) {
+  //function every time i have info from another server
+  console.log(data);
+  fill("red");
+  circle(data.x, data.y, 10);
+}
+
 function setup() {
-  createCanvas(400, 400);
+  background(220);
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(220);
+  fill("yellow");
   circle(mouseX, mouseY, 30);
+}
+
+function mouseMoved() {
+  let message = {
+    x: mouseX,
+    y: mouseY,
+  };
+
+  clientSocket.emit("mouse", message); //send to server con il nome "mouse"
 }
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 console.log("I'm running ");
 
-let express = require("express"); //we are putting the libriary in a var
+let express = require("express"); //we are putting the libriary in a var, express create server
 
 let app = express(); //app is the reslut of running the express funtion created before
 
@@ -16,11 +16,21 @@ let serverSocket = require("socket.io"); //creo variabile che contiene la librer
 
 let io = serverSocket(server); //Io = input/output diventa serverSocket con dentro la variabile che fa attivare express
 
-io.on("connection", newConnection); //se un messaggio connection arriva allora reagisce con la funzione newConnection
+io.on("connection", newConnection);
+//se un messaggio connection arriva allora reagisce con la funzione newConnection
 //we have to add on the client side the connection part otherwise we it won't happen nothing - go on index and sketch
 //every time a client connect we will have the function newConnection ,
 //ogni persona che si connette io vedrò nel console log un nuovo newSocket.id
 
 function newConnection(newSocket) {
   console.log(newSocket.id); //contains the unique id of client connection
+
+  newSocket.on("mouse", mouseMessage); //newsocket is the connection, quando ricevo un messaggio di tipo "mouse" allora execute funzione mouseMessage che contiene il data Received
+
+  function mouseMessage(dataReceived) {
+    //datarecived è ciò che viene creato sul server quando ricevo mouse dal client
+    console.log(dataReceived);
+
+    newSocket.broadcast.emit("mouseBroadcast", dataReceived); //boradcast send the emssage to all others except the one the send us the message
+  }
 }
